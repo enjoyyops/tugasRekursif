f200f4d9423402cb6f57fffc22ba98b988910e6a
enjoyyops <121224708@smkn1gunungguruh.sch.id>
2024-12-22 22:58:34 +0700

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,14 @@
+public class reverse {
+    public static void printReverse(int n){
+        if(n<1){
+            return;
+        }else{
+            System.out.print(n+" ");
+            printReverse(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printReverse(n);
+    }
+}
